e35936396d2059fa72fcce384d495621afe79e81
Artem <wieredsh@gmail.com>
2026-05-22 21:46:21 +0300

    refactor(main): add docstring

--- src/ruzsite/main.py
+++ src/ruzsite/main.py
@@ -7,5 +7,7 @@ from ruzsite.settings import get_settings
 
 settings = get_settings()
 
-if __name__ == "__main__":
+
+def main():
+    """Run the development server."""
     uvicorn.run("ruzsite.main:app", host=settings.host, port=settings.port, reload=True)
